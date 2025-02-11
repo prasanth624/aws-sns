94fcd52f2098dc285720358a6d9bcd712cd3ccfe
Prasanth B <125499600+prasanth624@users.noreply.github.com>
2025-02-12 00:38:43 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,10 @@
-provider "aws" {
+terraform {
+  required_version = ">= 1.10.0"
 
-  region = "us-east-1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
 }
